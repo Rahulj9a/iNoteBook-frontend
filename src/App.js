0e9b51aac4dd801c6fc940de3ab7c8e6c45d696a
Rahul Solanki <rahul.solanki.12032001@gmail.com>
2023-01-23 18:01:38 +0530

    unsuccessfull to add react router dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
- 
-import './App.css';
-
+import "./App.css";
+import React from "react";
+import {
+     createBrowserRouter,
+     createRoutesFromElements,
+     Route,
+} from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Home from "./Components/Home";
+import About from "./Components/About";
 function App() {
-  return (
-    <div className="App">
-      This is iNoteBook
-    </div>
-  );
+     const router = createBrowserRouter(
+     createRoutesFromElements(
+          <Route
+               exact
+               path="/"
+               element={<Navbar />}
+          >
+               ,
+               <Route
+                    index
+                    element={Home}
+               />
+               <Route
+                    index
+                    element={About}
+               />
+          </Route>,
+     ));
+     return (
+          <>
+               <Navbar />
+          </>
+     );
 }
 
 export default App;

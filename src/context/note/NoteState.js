7b154437cd9f3b63affad8f37e57246bf7cf365b
Rahul Solanki <rahul.solanki.12032001@gmail.com>
2023-02-21 19:59:22 +0530

    complete server side delete note

--- src/context/note/NoteState.js
+++ src/context/note/NoteState.js
@@ -33,7 +33,8 @@ const NoteState = (props) => {
      };
      //Delete a note:-
      const deleteNote = (id) => {
-          console.log(id)
+          const newNotes = notes.filter(note => { return note._id !== id })
+          setNotes(newNotes)
      };
 
      //Edit a note:-
